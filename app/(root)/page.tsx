06f0fb1138b2983cae7ebd4ed3b8c2b929a08e01
PhDBui <congdoan6997@gmail.com>
2024-07-20 13:41:49 +0300

    feat: 🎸 home page
    create room liveblocks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,42 @@
+import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
+import Image from "next/image";
+import AddDocumentButton from "@/components/AddDocumentButton";
+const HomePage = async () => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect("/sign-in");
 
-const HomePage = () => {
-  return <Button>Hello</Button>;
+  return (
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          {/* <Notifications /> */}
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+
+      <div className="document-list-empty">
+        <Image
+          src="/assets/icons/doc.svg"
+          alt="Document"
+          width={40}
+          height={40}
+          className="mx-auto"
+        />
+
+        <AddDocumentButton
+          userId={clerkUser.id}
+          email={clerkUser.emailAddresses[0].emailAddress}
+        />
+      </div>
+    </main>
+  );
 };
 
 export default HomePage;
